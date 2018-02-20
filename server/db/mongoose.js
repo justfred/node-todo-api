bd47aaac5edb5da05b0a3094c8cda890943f5ad0
Frederick Heald <fheald@bandwidthx.com>
2018-02-20 10:57:28 -0800

    PATCH todos

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
-//use the global promise library, not a third party
 mongoose.Promise = global.Promise;
-//connect to the db
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
