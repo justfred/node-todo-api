3050d782cf260987037d088082b2f4cc331e027d
Frederick Heald <fheald@bandwidthx.com>
2018-02-20 13:54:07 -0800

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,28 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+//statics are model methods not instance methods
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
